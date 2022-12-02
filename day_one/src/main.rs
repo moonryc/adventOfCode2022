25a3e886863b5e6eff5237ed6a2ca074625e2656
Ryan Moon <ryanmoondevelopment@gmail.com>
2022-12-01 20:17:05 -0600

    optimized code

--- day_one/src/main.rs
+++ day_one/src/main.rs
@@ -1,60 +1,40 @@
-use std::env;
+
 use std::fs::File;
-use std::io::{self, prelude::*, BufReader};
-use std::str::FromStr;
+use std::io::{BufReader, prelude::*};
 
 
-
-
-
-fn part_one(file_name: &str) -> i32{
-    let file = File::open(file_name).expect("expected a valid file");
-    let reader = BufReader::new(file);
-
-    let mut currentIndex = 0;
-    let mut maxIndex = 0;
-    let mut maxTotal:i32 = 0;
-    let mut currentTotal = 0;
-    for line in reader.lines(){
-        let unwrapped_line = line.unwrap();
-        if unwrapped_line.is_empty() {
-            if currentTotal > maxTotal {
-                maxTotal = currentTotal;
-                maxIndex = currentIndex;
-            }
-            currentIndex = currentIndex +1;
-            currentTotal = 0;
-        } else{
-            let guess: i32 = match unwrapped_line.trim().parse(){
-                Ok(num)=> num,
-                Err(_)=>continue,
-            };
-            currentTotal = currentTotal + guess;
-        }
-
-    }
-    return maxTotal;
-}
-
-fn part_two(file_name:&str)->i32{
+fn file_to_vector(file_name: &str)->Vec<i32>{
     let file = File::open(file_name).expect("expected a valid file");
     let reader = BufReader::new(file);
 
     let mut vector: Vec<i32> = Vec::new();
-    let mut maxAmount = 0;
+    let mut max_amount = 0;
     for line in reader.lines(){
         let unwrapped_line = line.unwrap();
         if unwrapped_line.is_empty() {
-            vector.push(maxAmount);
-            maxAmount = 0;
+            vector.push(max_amount);
+            max_amount = 0;
         }else{
             let number: i32 = unwrapped_line.parse().unwrap();
-           maxAmount = maxAmount + number;
+            max_amount = max_amount + number;
         }
     }
-    vector.sort();
-    vector.reverse();
-    return vector[0] + vector[1] + vector[2];
+    vector
+}
+
+
+fn part_one(file_name: &str) -> i32{
+    let mut summarized_calories_per_elf = file_to_vector(file_name);
+    summarized_calories_per_elf.sort();
+    summarized_calories_per_elf.reverse();
+    summarized_calories_per_elf[0]
+}
+
+fn part_two(file_name:&str)->i32{
+    let mut summarized_calories_per_elf = file_to_vector(file_name);
+    summarized_calories_per_elf.sort();
+    summarized_calories_per_elf.reverse();
+    return summarized_calories_per_elf[0] + summarized_calories_per_elf[1] + summarized_calories_per_elf[2];
 }
 
 fn main() {
@@ -62,6 +42,6 @@ fn main() {
     let answer_one = part_one(file_name);
     let answer_two = part_two(file_name);
     println!("DAY ONE");
-    println!("    Part one: {answer_one}");
-    println!("    part two: {answer_two}");
+    println!("|-> Part One: {answer_one}");
+    println!("|-> Part Two: {answer_two}");
 }
